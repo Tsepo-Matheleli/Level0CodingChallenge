183b944348a2fe3bc3789a33f498c50bf2601a2d
Tsepo Matheleli <tsepomatheleli@icloud.com>
2021-03-16 22:10:00 +0200

    Added negative number handling to function

--- Task 0.6.py
+++ Task 0.6.py
@@ -1,10 +1,17 @@
-def maximum(*bonus_values):       # Unpack parameter list
+def maximum(*bonus_values):
     maximum_value = 0
     for item in bonus_values:
-        if maximum_value < item:
+        if maximum_value < item and item >= 0:
             maximum_value = item
+        else:
+            maximum_value = item
+            for negative_value in bonus_values:
+                if maximum_value < negative_value:
+                    maximum_value = negative_value
     return maximum_value
 
 
-bonus_value = maximum(1,22,3,2)
+bonus_value = maximum(1, 22, 3, 2)
+bonus_negative_value = maximum(-1000, -1, -10, -100)
 print('Bonus Maximum Value: ' + str(bonus_value))
+print('Bonus Maximum Negative Value: ' + str(bonus_negative_value))
