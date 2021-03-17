e8f6af0f3b90e9ea4c2ba4bef399cccf9f06808c
Tsepo Matheleli <tsepomatheleli@icloud.com>
2021-03-17 17:55:06 +0200

    Made required output changes for 0 minutes

--- Task 0.8.py
+++ Task 0.8.py
@@ -1,22 +1,15 @@
 def time(number):
-    if number >= 60:
-        hour = number // 60
-        if number % 60 > 0:
-            minute = number % 60
-            if hour > 1 and minute > 1:
-                print(str(hour) + ' hours, ' + str(minute) + " minutes")
+    hour = number // 60
+    minute = number % 60
+    if hour > 1 and minute >= 0:
+        print(str(hour) + ' hours, ' + str(minute) + " minutes")
 
-            elif hour == 1 and minute > 1:
-                print(str(hour) + ' hour, ' + str(minute) + " minutes")
-            elif hour == 1 and minute == 1:
-                print(str(hour) + ' hour, ' + str(minute) + " minute")
-            else:
-                print(str(hour) + ' hours, ' + str(minute) + " minute")
-        else:
-            if hour == 1:
-                print(str(hour) + ' hour')
-            else:
-                print(str(hour) + ' hours')
+    elif hour == 1 and minute >= 0:
+        print(str(hour) + ' hour, ' + str(minute) + " minutes")
+    elif hour == 1 and minute == 1:
+        print(str(hour) + ' hour, ' + str(minute) + " minute")
+    elif hour == 1 and minute == 1:
+        print(str(hour) + ' hours, ' + str(minute) + " minute")
     else:
         if number == 1:
             print(str(number) + ' minute')
@@ -26,5 +19,6 @@ def time(number):
 
 time(75)
 time(105)
-time(59)
+time(120)
+time(50)
 time(250)
